5d6074a0e9b9f239163fae18859ed0d69be5f975
Kaung223 <kaungpyaeaung8123@gmail.com>
2025-03-03 16:43:00 +0630

    feat: add animation on the HomeIntro

--- src/features/Home/components/HomeIntro.tsx
+++ src/features/Home/components/HomeIntro.tsx
@@ -32,20 +32,67 @@ const HomeIntro = () => {
     },
   ];
 
+  useEffect(() => {
+    const IntroTimeLine1 = gsap.timeline({
+      defaults: { ease: "power2" },
+      scrollTrigger: {
+        trigger: ".IntroContainer",
+        start: "65% bottom",
+        markers: true,
+      },
+    });
+
+    const IntroTimeLine2 = gsap.timeline({
+      defaults: { ease: "power1" },
+      scrollTrigger: {
+        trigger: ".IntroContainer",
+        start: "65% bottom",
+        markers: true,
+      },
+    });
+
+    // Step 1: Animate IntroSidePhoto first
+    IntroTimeLine1.from(".IntroSidePhoto", {
+      x: 300,
+      duration: 3,
+      opacity: 0,
+    });
+
+    IntroTimeLine2.from(".IntroTitle", {
+      y: 50,
+      duration: 0.7,
+      opacity: 0,
+      stagger: 0.5,
+    }).from(".IntroCard", {
+      y: 50,
+      duration: 0.7,
+      stagger: {
+        amount:0.5,
+        grid: [2,2], 
+        axis: "y", 
+      },
+
+      duration: 1,
+      opacity: 0,
+    });
+  }, []);
+
   return (
-    <Container className={"grid grid-cols-3 gap-3 py-24"}>
-      <div className="col-span-2">
-        <p className="text-4xl tracking-wider font-medium font-poppins">
+    <Container className={"grid grid-cols-3 gap-3 py-24 overflow-hidden"}>
+      <div className="col-span-2 IntroContainer">
+        <p className="text-4xl IntroTitle tracking-wider font-medium font-poppins">
           Japan, A Land of Timeless Beauty
         </p>
-        <p className="text-xl mt-4">( 永遠の美を誇る国、日本 )</p>
-        <p className="mt-9 pe-12 text-lg">
+        <p className="text-xl IntroTitle mt-4">( 永遠の美を誇る国、日本 )</p>
+        <p className="mt-9 pe-12 text-lg IntroTitle">
           We offer a variety of exciting travel packages, from breathtaking
           natural scenery to historic cultural experiences across Japan.
         </p>
         <div className="mt-20 grid grid-cols-2 gap-3">
           {IntroCardContent.map((content) => (
-            <IntroCard content={content} />
+            <div className="bg-neutral-100 IntroCard">
+              <IntroCard content={content} />
+            </div>
           ))}
         </div>
       </div>
@@ -54,7 +101,7 @@ const HomeIntro = () => {
           backgroundImage:
             "url(https://images.unsplash.com/photo-1610894377860-d73415b792b2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZ1aml8ZW58MHx8MHx8fDA%3D)",
         }}
-        className="p-3 group overflow-hidden"
+        className="p-3 group overflow-hidden IntroSidePhoto h-full"
       >
         <div className="opacity-0 -translate-y-[50%] group-hover:opacity-100 group-hover:translate-y-0 duration-300">
           <IntroCard
@@ -75,9 +122,7 @@ const IntroCard = ({ content }) => {
     <div className="p-6 bg-neutral-100">
       <p className="text-2xl font-poppins font-medium">{content.title}</p>
       <p className="mt-2 text-sm">( {content.japan_title} )</p>
-      <p className="text-justify mt-6 text-gray-800">
-        {content.content}.
-      </p>
+      <p className="text-justify mt-6 text-gray-800">{content.content}.</p>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { RouterProvider } from "react-router";
 import router from "./routes/router";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger);
+
 
 createRoot(document.getElementById("root")!).render(
   
